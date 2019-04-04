bc28b56f1df7e1b0b0497aee4ed67849373c9610
Agliullin Dmitry <dmitry.agli@gmail.com>
2019-04-04 10:25:08 +0500

    Change db url

--- src/connectDB.js
+++ src/connectDB.js
@@ -4,7 +4,7 @@ let client
 
 async function connectDB() {
     // const url = 'mongodb://localhost:27017/';
-    const url = 'mongodb://heroku_qzd1d9lm:mLab328654@ds131676.mlab.com:31676/heroku_qzd1d9lm'
+    const url = 'mongodb://admin:mLab328654@ds131676.mlab.com:31676/heroku_qzd1d9lm'
     // const dbname = 'apollo-tutorial-test';
     const dbname = 'heroku_qzd1d9lm';
     
